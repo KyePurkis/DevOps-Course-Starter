87108f33cceb0636f90cd531429b2557117ca493
Kye Purkis <kye.purkis@NECSWS.com>
2022-03-22 12:12:41 +0000

    Displayed todo list in index.html, added new route to handle new form in index.html for creating new todo items.

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request
 
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,4 +9,12 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    return render_template("index.html", items=items)
+
+@app.route('/item', methods=["POST"])
+def create_item():
+    item_title = request.form.get('item_title')
+    print(item_title)
+    add_item(item_title)
+    return redirect('/')
