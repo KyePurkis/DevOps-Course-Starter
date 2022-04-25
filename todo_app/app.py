eaef3621e795d0170c7d8ef7a7b60f94e7587437
Kye Purkis <kye.purkis@apdcomms.co.uk>
2022-04-25 16:39:20 +0100

    Added changes to allow items to be set to done.

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,8 +1,7 @@
 from flask import Flask, redirect, render_template, request
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import add_item
-from todo_app.data.trello_items import get_items
+from todo_app.data.trello_items import get_items, add_item, complete_item as complete_trello_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -11,11 +10,15 @@ app.config.from_object(Config())
 @app.route('/')
 def index():
     items = get_items()
-    print(items[2].status)
     return render_template("index.html", items=items)
 
 @app.route('/item', methods=["POST"])
 def create_item():
     item_title = request.form.get('item_title')
     add_item(item_title)
+    return redirect('/')
+
+@app.route('/item/<id>/complete')
+def complete_item(id):
+    complete_trello_item(id)
     return redirect('/')
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -7,11 +7,26 @@ TOKEN = os.getenv('TOKEN')
 BOARD_ID = os.getenv('BOARD_ID')
 BASE_URL = 'https://api.trello.com/1'
 
+TODO_LIST_NAME = 'To Do'
+DOING_LIST_NAME = 'Doing'
+DONE_LIST_NAME = 'Done'
+
 def get_items():
-    result = requests.get(f'{BASE_URL}/boards/{BOARD_ID}/lists', params={ 'cards': 'open', 'key': API_KEY, 'token': TOKEN})
-    lists = result.json()
+    lists = requests.get(f'{BASE_URL}/boards/{BOARD_ID}/lists', params={ 'cards': 'open', 'key': API_KEY, 'token': TOKEN}).json()
     data = []
     for l in lists:
         for card in l['cards']:
             data.append(Item.from_trello_card(card, l))
     return data
+
+def add_item(name):
+    todo_list_id = get_list_id(TODO_LIST_NAME)
+    result = requests.post(f'{BASE_URL}/cards', params={'idList': todo_list_id, 'name': name, 'key': API_KEY, 'token': TOKEN})
+
+def complete_item(id):
+    done_list_id = get_list_id(DONE_LIST_NAME)
+    result = requests.put(f'{BASE_URL}/cards/{id}', params={'idList': done_list_id, 'key': API_KEY, 'token': TOKEN})
+
+def get_list_id(name):
+    lists = requests.get(f'{BASE_URL}/boards/{BOARD_ID}/lists', params={ 'key': API_KEY, 'token': TOKEN}).json()
+    return next(x['id'] for x in lists if x['name'] == name)
